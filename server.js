3853127ad1824b5a89f26dc722bd357baec7d3db
Neha <neha7895928593@gmail.com>
2024-11-04 17:06:35 +0530

    some changes in server.js

--- server.js
+++ server.js
@@ -7,15 +7,12 @@ dotenv.config();
 const app = express();
 
 const productRoutes = require('./routes/productRoutes');
-const checkoutRoutes = require('./routes/checkOutRoutes');
-const paymentRoutes = require('./routes/paymentRoutes');
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api/products', productRoutes);
-app.use('/api/checkout', checkoutRoutes);
-app.use('/api/payment', paymentRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
